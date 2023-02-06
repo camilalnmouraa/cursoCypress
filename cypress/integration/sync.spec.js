b9bda3d7a01427724aa44cf4f2484e6c87cabb13
Luiza Gusmao de Andrade <andradeg.luiza@outlook.com>
2023-02-05 21:17:42 -0300

    feat: trabalhei com espera

---
+++ cypress/integration/sync.spec.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+describe('Wait', () => {
+
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+
+    it('Shoud wait for element to be available', () => {
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#novoCampo').should('exist')
+        cy.get('#novoCampo').type('Funciona')
+    })
+
+})
